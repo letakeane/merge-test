4069ffd3e8589d6460194d1163e126e293cfcf59
Leta Keane <leta@hey.com>
2023-10-17 09:45:47 -0600

    Rename fns, refactor syntax

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var array = ["Ally", "Brennan", "Emily", "Lou", "Murph", "Sioban", "Zac"];
+var guestNames = ["Ally", "Brennan", "Emily", "Lou", "Murph", "Sioban", "Zac"];
 
 var guest = {
   name: "Erika",
   favCandy: ['Snickers', 'candy corn']
 }
 
-function numbers(num1, num2) {
-  var sum = num1 + num2
+function addTwoNumbers(num1, num2) {
+  var sum = num1 + num2;
   return sum;
 }
 
-function candy(obj) {
-return obj.name + ' likes ' + obj.favCandy[0] + ' and ' + obj.faveCandy[1];
+function makeCandyStatement(obj) {
+  return obj.name + ' likes ' + obj.favCandy[0] + ' and ' + obj.faveCandy[1];
 }
 
-function allGuests(array) {
-for (var i = 0; i < array.length; i++) {
-  console.log('Welcome to the party, ' + array[i])
-}
-}
+function welcomeGuests(array) {
+  for (var i = 0; i < array.length; i++) {
+    console.log('Welcome to the party, ' + array[i]);
+  }
+} 
